6145b331cb593d646445d52d091e7a0b7412aad5
wonjinsong <msmk22239@gmail.com>
2021-01-19 17:35:10 -0500

    class component 2

--- rpsClass.jsx
+++ rpsClass.jsx
@@ -3,19 +3,35 @@ import React, { Component } from 'react';
 // (setState/props 바뀔때 -> shouldComponentUpdate(true일때) ->rerender -> componentDidUpdate)
 // (부모가 나를 없앴을때 -> componentWillUnmount -> 소멸) 
 
+const rpsCoods = {
+    rock : '0',
+    scissors : '-142px',
+    paper : '-284px',
+};
+
+const scores = {
+    scissors : 1,
+    rock : 0,
+    paper : -1,
+};
 class Rps extends Component {
     state = {
         result : '',
         imgCoord : 0,
         score : 0,
     };
-    //쌍으로 한다. 비동기요청에 많이 사용됨
-    componentDidMount() {  //첫 랜더링 직후에
+    interval;
 
+    //쌍으로 한다. 비동기에 많이 사용됨
+    componentDidMount() {  //첫 랜더링 직후에, 비동기요청
+        const {imgCoord} = this.state;
+        this.interval = setInterval(()=> {
+
+        }, 1000);
     }
 
-    componentWillUnmount() {  // 컴포넌트 제거되기 직전
-
+    componentWillUnmount() {  // 컴포넌트 제거되기 직전, 비동기정리(취소)
+        clearInterval(this.interval);
     }
 
     render() {
