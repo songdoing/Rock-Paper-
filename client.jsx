ea27c806e654e777ef727d7dbf89027d9382db6f
wonjinsong <msmk22239@gmail.com>
2021-01-20 07:50:07 -0500

    componentDidMount setInterval

--- client.jsx
+++ client.jsx
@@ -1,6 +1,6 @@
-const React = require('react');
-const ReactDom = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const Rps = require('./rpsClass');
+import Rps from './rpsClass';
 
-ReactDom.render(<Rps/>, document.querySelector('#root'));
+ReactDOM.render(<Rps />, document.querySelector('#root'));
--- rpsClass.jsx
+++ rpsClass.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 // (setState/props 바뀔때 -> shouldComponentUpdate(true일때) ->rerender -> componentDidUpdate)
 // (부모가 나를 없앴을때 -> componentWillUnmount -> 소멸) 
 
-const rpsCoods = {
+const rpsCoords = {
     rock : '0',
     scissors : '-142px',
     paper : '-284px',
@@ -17,23 +17,42 @@ const scores = {
 class Rps extends Component {
     state = {
         result : '',
-        imgCoord : 0,
+        imgCoord : rpsCoords.rock,
         score : 0,
     };
+
     interval;
 
     //쌍으로 한다. 비동기에 많이 사용됨
-    componentDidMount() {  //첫 랜더링 직후에, 비동기요청
-        const {imgCoord} = this.state;
+    componentDidMount() {  //첫 랜더링 직후에, 비동기요청             
         this.interval = setInterval(()=> {
-
-        }, 1000);
+            //비동기 함수가 그 밖에 있는 변수를 참조하면 클로져 발생
+            //비동기 함수 안에 변수처리함.   
+            const {imgCoord} = this.state;
+            if(imgCoord === rpsCoords.rock) {
+                this.setState({
+                    imgCoord : rpsCoords.scissors,
+                });
+            } else if(imgCoord === rpsCoords.scissors) {
+                this.setState({
+                    imgCoord : rpsCoords.paper,
+                });
+            } else if (imgCoord === rpsCoords.paper) {
+                this.setState({
+                    imgCoord : rpsCoords.rock,
+                });
+            }
+        }, 100);
     }
 
     componentWillUnmount() {  // 컴포넌트 제거되기 직전, 비동기정리(취소)
         clearInterval(this.interval);
     }
 
+    onClickBtn = (choice) => {
+
+    };
+    //render안에는 setState들어가면 무한...반복..안됨!
     render() {
         const { result, score, imgCoord } =this.state;
         return (
